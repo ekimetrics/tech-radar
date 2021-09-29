982f487d5cf4db1f96d6f3f142baa61cfb7cacf7
Theo Alves Da Costa <theo.alvesdacosta@ekimetrics.com>
2021-09-29 14:47:48 +0200

    Sept 2021 version

--- docs/make_data.py
+++ docs/make_data.py
@@ -7,11 +7,12 @@ PATH = f"C:/Users/{USER}/Ekimetrics/Ekimetrics. - Eki.Innovation/Opensource Tech
 
 data = pd.read_excel(PATH).iloc[:,:6]
 
-data["quadrant"] = data["quadrant"].replace({"Data Science":2,"Data Engineering":3,"Web Development":1,"Software Development":0})
+data["quadrant"] = data["quadrant"].str.lower().str.replace(" ","").replace({"datascience":2,"dataengineering":3,"applicationdevelopment":1,"ops":0})
 data["ring"] = data["ring"].replace({"ADOPT":0,"TRIAL":1,"ASSESS":2,"HOLD":3})
 data["active"].fillna(True,inplace = True)
 data["moved"].fillna(0,inplace = True)
 data.drop("url",axis = 1,inplace = True)
+data = data.drop_duplicates(subset = ["label"])
 
 json_data = data.to_json(orient = "records")
 js_data = f"""
